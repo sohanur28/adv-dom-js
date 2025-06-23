41999ccd0dae42f13f3791c24579e40007c4da3b
sohanur28 <sohanur28@gmail.com>
2025-06-23 10:39:23 +0600

    first commit

---
+++ validation.js
@@ -0,0 +1,10 @@
+const number = 23;
+// string, number, boolean
+if(typeof number === 'number'){
+    console.log('value is a number');
+    
+}
+else{
+    console.log('value is not a number');
+    
+}
