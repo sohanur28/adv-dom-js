d52f501443adc2bdd4f03a7dd21b6ec7243c6dd6
sohanur28 <sohanur28@gmail.com>
2025-06-23 10:55:16 +0600

    isNaN

--- validation.js
+++ validation.js
@@ -8,3 +8,12 @@ else{
     console.log('value is not a number');
     
 }
+
+const numbers = [3, 12, 78, 90];
+const student = {name: 'Rongila Rafsan', job: 'Khawadawa kora'}
+// console.log(Array.isArray(numbers));
+// console.log(Array.isArray(number));
+// console.log(typeof student);
+console.log(isNaN(student));
+console.log(isNaN(number));
+console.log(isNaN(numbers));
